2845b6fa268693c77b4d3989da8029b9f8562db6
I-koinonos <wisdom.okechukwu1998@gmail.com>
2021-02-26 11:13:16 -1100

    commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\PostLiked;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+
+class DashboardController extends Controller
+{
+
+    public function __construct()
+    {  
+        $this->middleware(['auth']);       
+    }
+
+    public function index(){
+
+        
+        
+        
+        return view('dashboard');
+    }
+}
